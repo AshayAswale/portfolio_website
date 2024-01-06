b6275580670f4f43ba51461a5e5d1efba12c3452
Ashay Aswale <asaswale@wpi.edu>
2024-01-06 12:32:08 -0500

    Working NavBar

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+import { AshayLogo } from "./Icons";
+
+const Logo = () => {
+  return (
+  <div className="flex items-center justify-center mt-2">
+    <Link href={"/"}>
+    AA logo
+    </Link>
+  </div>
+  );
+};
+
+export default Logo;
